b1b022a29f4f50be8a12229e58c18b91938e4264
adriana.del.teso <adriana.del.teso@accenture.com>
2021-10-01 12:40:26 +0200

    :bento: Rename events-app files

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'events-app',
   template: '<h2> Holi, Mundo </h2>>',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./events-app.component.scss']
 })
 export class EventsAppComponent {
   title = 'ng-events-db';
