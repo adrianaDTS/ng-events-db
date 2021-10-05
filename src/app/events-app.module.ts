b81bc2f7a9592f6e07302ddcb990579b1b7717c0
adriana.del.teso <adriana.del.teso@accenture.com>
2021-10-05 09:18:46 +0200

    :clown_face: Add mock service for events

--- src/app/events-app.module.ts
+++ src/app/events-app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventsThumbnailComponent } from './events/events-thumbnail.component';
+import { EventService } from './events/shared/event.service';
 import { NavBarComponent } from './nav/navbar-component';
 
 @NgModule({
@@ -21,7 +22,7 @@ import { NavBarComponent } from './nav/navbar-component';
   ],
 
   // Services are added as providers
-  providers: [],
+  providers: [EventService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
