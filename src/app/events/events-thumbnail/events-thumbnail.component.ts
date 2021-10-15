d2bb7570a26068381adc39793d99748688005800
adriana.del.teso <adriana.del.teso@accenture.com>
2021-10-15 13:15:51 +0200

    :zap: Add pipes to improve formats

--- src/app/events/events-thumbnail/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail/events-thumbnail.component.ts
@@ -5,26 +5,7 @@ import { IEvent } from '../index';
 
 @Component({
   selector: 'event-thumbnail',
-  template: `
-  <article [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
-    <h2 h2 > {{ event?.name }}</h2>
-    <p> Date: {{ event?.date }}</p>
-    <p [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time"> Time: {{ event?.time }}
-      <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
-      <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
-      <span *ngSwitchDefault>(Normal Start)</span>
-    </p>
-
-    <p> Price: {{ event?.price }} €</p>
-    <!-- The p element is not render if the location doesn't exits -->
-    <p *ngIf="event?.location">
-    <span>Location: {{ event?.location?.address }},</span>
-    <span class="pad-left">{{ event?.location?.city }}, {{ event?.location?.country }}</span>
-    </p>
-    <!--  The p element is hidden if the onlineUrl doesn't exist -->
-    <p [hidden]="!event?.onlineUrl">Online URL:<a [href]="event?.onlineUrl" class="pad-left">{{ event?.onlineUrl }}</a> </p>
-  </article>
-  `,
+  templateUrl: './events-thumbnail.component.html',
   styles: [`
     .green { color: #003300 !important; }
     .bold { font-weight: bold; }
