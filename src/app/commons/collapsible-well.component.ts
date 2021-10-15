561335ec21035d60a4329181071d4f6e9f9cb7ba
adriana.del.teso <adriana.del.teso@accenture.com>
2021-10-15 08:30:13 +0200

    :lipstick: Improve style in collapsible-well component

--- src/app/commons/collapsible-well.component.ts
+++ src/app/commons/collapsible-well.component.ts
@@ -1,17 +1,26 @@
 import { Component, Input } from "@angular/core";
+import {faPlusCircle, faMinusCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'collapsible-well',
   template: `
-  <div (click)="toggleContent()" class="well pointable">
-    <h4 class="well-title">{{title}}</h4>
+  <div (click)="toggleContent()" class="well pointable well-collapsible">
+    <div class="well-header">
+      <h4 class="well-title">{{title}}</h4>
+      <fa-icon *ngIf="!visible" [icon]="faPlusCircle"></fa-icon>
+      <fa-icon *ngIf="visible" [icon]="faMinusCircle"></fa-icon>
+    </div>
     <ng-content *ngIf="visible"></ng-content>
-  </div>`
+  </div>`,
+  styleUrls: ['./collapsible-well.component.scss']
 })
 
 export class CollapsibleWellComponent {
   @Input() title: string;
-  visible: boolean = true;
+  visible: boolean = false;
+
+  faPlusCircle = faPlusCircle;
+  faMinusCircle = faMinusCircle;
 
   toggleContent() {
     this.visible = !this.visible;
