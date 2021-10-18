7e32688fdb374e4ea128e4bbdf32a03aef27db83
adriana.del.teso <adriana.del.teso@accenture.com>
2021-10-18 09:56:11 +0200

    :art: Add filter and sorting methods

--- src/app/events/session-list/session-list.component.ts
+++ src/app/events/session-list/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges } from "@angular/core";
 import { ISession } from "../shared/index";
 import { faFire } from '@fortawesome/free-solid-svg-icons';
 
@@ -13,8 +13,49 @@ import { faFire } from '@fortawesome/free-solid-svg-icons';
 
 })
 
-export class SessionListComponent {
+export class SessionListComponent implements OnChanges {
   @Input() sessions: ISession[];
+  @Input() filterBy: string;
+  @Input() sortBy: string;
+  visibleSessions: ISession[] = [];
 
+  // This method (PnChanges) is going to be called everytime a value changes
+  ngOnChanges() {
+    if (this.sessions) {
+      this.filterSession(this.filterBy);
+      this.sortBy === 'name'
+        /* sort is a mutating method, it doesn't create a new array,
+        it sorted it in place */
+        ? this.visibleSessions.sort(sortByNameAsc)
+        : this.visibleSessions.sort(sortByVotesDesc);
+    }
+  }
+
+  filterSession(filter) {
+    if (filter === 'all') {
+      // the original array of sessions needs to be cloned. The slice method could create a duplicate of it.
+      this.visibleSessions = this.sessions.slice(0);
+    } else {
+      // here is created a complete new array with the filtered sessions
+      this.visibleSessions = this.sessions.filter(session => {
+        return session.level.toLocaleLowerCase() === filter;
+      });
+    }
+  }
+
+  // Icons
   faFire = faFire;
+}
+
+/* The stateless functions don't need to be inside the class,
+they can be mplemented outside of it. */
+
+function sortByNameAsc(s1: ISession, s2: ISession) {
+  if (s1.name > s2.name) { return 1; }
+  else if (s1.name === s2.name) { return 0; }
+  else { return -1; };
+}
+
+function sortByVotesDesc(s1: ISession, s2: ISession) {
+  return s2.voters.length - s1.voters.length;
 }
