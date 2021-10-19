83ebf745f3b5023b771ed1578271781d86f82533
adriana.del.teso <adriana.del.teso@accenture.com>
2021-10-19 09:00:16 +0200

    :truck: Separete styles from code in session list component

--- src/app/events/session-list/session-list.component.ts
+++ src/app/events/session-list/session-list.component.ts
@@ -6,10 +6,7 @@ import { faFire } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'session-list',
   templateUrl: './session-list.component.html',
-  styles: [`
-  .well-title {transition: 0.4s linear all;}
-  .well-title:hover { transform: scale(1.04);}
-  .well-title fa-icon {margin-left:10px; color: #f97924}`]
+  styleUrls: ['session-list.component.scss']
 
 })
 
